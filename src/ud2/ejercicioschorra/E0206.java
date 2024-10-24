92b22c7b4a4629cbf5c3917f3515c71ce495af13
JoseChapela <jchappast@ieschandomonte.edu.es>
2024-10-24 18:06:24 +0200

    x

--- src/ud2/ejercicioschorra/E0206.java
+++ src/ud2/ejercicioschorra/E0206.java
@@ -4,7 +4,7 @@ package ud2.ejercicioschorra;
 
 import java.util.Scanner;
 
-public class EP206 {
+public class E0206 {
      public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
