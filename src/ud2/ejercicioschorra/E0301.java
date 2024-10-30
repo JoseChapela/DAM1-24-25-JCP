b9d956c97da2d96244b7a6c7d93970eaa3847b7c
JoseChapela <jchappast@ieschandomonte.edu.es>
2024-10-30 10:36:58 +0100

    30/10_10:36

---
+++ src/ud2/ejercicioschorra/E0301.java
@@ -0,0 +1,57 @@
+/**E0301. Diseñar un programa que muestre, para cada número introducido por teclado, si
+es par, si es positivo y su cuadrado. El proceso se repetirá hasta que el número
+introducido sea 0.
+ */
+
+package ud2.ejercicioschorra;
+
+import java.util.Scanner;
+
+public class E0301 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner (System.in);
+        int num, cuadrado;
+        boolean par, positivo;
+        
+        System.out.println("PAR? POSITIVO? CUADRADO?");
+        System.out.println("Introduce un número");
+        num = sc.nextInt();
+
+         while (num!=0) {
+
+            par = num%2==0;
+            positivo = num >=0;
+            cuadrado = num*num;
+
+            if (par) {
+                System.out.println("Es par");
+                
+            } else {
+                System.out.println("Es impar");
+
+            }
+
+            if (positivo) {
+                System.out.println("Es positivo");
+                
+            } else {
+                System.out.println("Es negativo");
+
+            }
+            System.out.println("Su cuadrado es "+cuadrado);
+
+            System.out.println("Introduce un número");
+            num = sc.nextInt();
+
+         };
+         sc.close();
+
+           
+            
+        
+
+
+    }
+    
+}
