500f639423f521aea857b9191d4bb4a818086d4b
JoseChapela <jchappast@ieschandomonte.edu.es>
2024-10-30 13:13:36 +0100

    30/10_13:13

--- src/personal/ANIM.java
+++ src/personal/ANIM.java
@@ -3,106 +3,98 @@ package personal;
 public class ANIM {
     public static void main(String[] args) throws InterruptedException {
 
+        final int x = 10;
         int i = 0;
-
-        while (i<10) {
         
-        System.out.println("                                                                                            ");
-        System.out.println("                                                                                            ");
-        System.out.println("            * * * * * * * * * * * * * * * * * * * * * * * * * *                             ");
-        System.out.println("          *                                                     *                           ");
-        System.out.println("          *                                                     *                           ");
-        System.out.println("            * * * * * * * * * * * * * * * * * * * * * * * * * *                             ");
+        while (i<x) {
 
-        Thread.sleep(400);
+            String hop = "              ".repeat(i);
+            
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"            * * * * * * * * * * * * * * * * * * * * * * * * * *                             ");
+            System.out.println(hop+"          *                                                     *                           ");
+            System.out.println(hop+"          *                                                     *                           ");
+            System.out.println(hop+"            * * * * * * * * * * * * * * * * * * * * * * * * * *                             ");
 
-        System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
-       
-        System.out.println("                                                                                            ");
-        System.out.println("                                        * * * * * * * * *                               ");
-        System.out.println("                * * * * * * * * * * * *                   * * *                            ");
-        System.out.println("              *                                                 *                           ");
-        System.out.println("              *                           * * * * * *           *                     ");
-        System.out.println("                * * * * * * * * * * * * *             * * * * *                             ");
+            Thread.sleep(400);
 
-        Thread.sleep(400);
+            System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
+        
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                        * * * * * * * * *                               ");
+            System.out.println(hop+"                * * * * * * * * * * * *                   * * *                            ");
+            System.out.println(hop+"              *                                                 *                           ");
+            System.out.println(hop+"              *                           * * * * * *           *                     ");
+            System.out.println(hop+"                * * * * * * * * * * * * *             * * * * *                             ");
 
-        System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
+            Thread.sleep(400);
 
-        System.out.println("                                                                                            ");
-        System.out.println("                                                                                            ");
-        System.out.println("                                                                                            ");
-        System.out.println("                                            * *                                           ");
-        System.out.println("                                      * * *     * *                                       ");
-        System.out.println("                              * * * *               * *                                   ");
-        System.out.println("                  * * * * * *                           * * *                             ");
-        System.out.println("                *                         * * *               *                          ");
-        System.out.println("                *                 * * * *       *             *                           ");
-        System.out.println("                  * * * * * * * *                 * * * * * *                             ");
+            System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
 
-        Thread.sleep(400);
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                              * *                                           ");
+            System.out.println(hop+"                                        * * *     * *                                       ");
+            System.out.println(hop+"                                * * * *               * *                                   ");
+            System.out.println(hop+"                    * * * * * *                           * * *                             ");
+            System.out.println(hop+"                  *                         * * *               *                          ");
+            System.out.println(hop+"                  *                 * * * *       *             *                           ");
+            System.out.println(hop+"                    * * * * * * * *                 * * * * * *                             ");
 
-        System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
+            Thread.sleep(400);
 
-        System.out.println("                                                                                            ");
-        System.out.println("                                                                                            ");
-        System.out.println("                                          * *                                                     ");
-        System.out.println("                                        *     *                                                   ");
-        System.out.println("                                    * *         * *                                               ");
-        System.out.println("                              * * *                 * *                                           ");
-        System.out.println("                        * * *                            * *                                    ");
-        System.out.println("                      *                   * * *               *                                   ");
-        System.out.println("                      *           * * * *       *             *                                   ");
-        System.out.println("                        * * * * *                 * * * * * *                                     ");
+            System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
 
-        Thread.sleep(400);
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                            * *                                                     ");
+            System.out.println(hop+"                                          *     *                                                   ");
+            System.out.println(hop+"                                      * *         * *                                               ");
+            System.out.println(hop+"                                * * *                 * *                                           ");
+            System.out.println(hop+"                          * * *                            * *                                    ");
+            System.out.println(hop+"                        *                   * * *               *                                   ");
+            System.out.println(hop+"                        *           * * * *       *             *                                   ");
+            System.out.println(hop+"                          * * * * *                 * * * * * *                                     ");
 
-        System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
+            Thread.sleep(400);
 
-        System.out.println("                                                                                            ");
-        System.out.println("                                                                                            ");
-        System.out.println("                                                                                            ");
-        System.out.println("                                      * * *                                                             ");
-        System.out.println("                                  * *       * *                                                         ");
-        System.out.println("                              * *               * * *                                                   ");
-        System.out.println("                        * * *                         * * * * * *                                     ");
-        System.out.println("                      *               * *                         *                                   ");
-        System.out.println("                      *           * *     * *                     *                                   ");
-        System.out.println("                        * * * * *             * * * * * * * * * *                                     ");
+            System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
 
-        Thread.sleep(400);
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                        * * *                                                             ");
+            System.out.println(hop+"                                    * *       * *                                                         ");
+            System.out.println(hop+"                                * *               * * *                                                   ");
+            System.out.println(hop+"                          * * *                         * * * * * *                                     ");
+            System.out.println(hop+"                        *               * *                         *                                   ");
+            System.out.println(hop+"                        *           * *     * *                     *                                   ");
+            System.out.println(hop+"                          * * * * *             * * * * * * * * * *                                     ");
 
-        System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
+            Thread.sleep(400);
 
-        System.out.println("                                                                                            ");
-        System.out.println("                                                                                            ");
-        System.out.println("                                                                                            ");
-        System.out.println("                                                                                            ");
-        System.out.println("                                * * * * * * * *                                                         ");
-        System.out.println("                            * *                 * * * * *                                               ");
-        System.out.println("                        * *                               * * * * *                                     ");
-        System.out.println("                      *                                             *                                   ");
-        System.out.println("                      *         * * * * * * * *                     *                                   ");
-        System.out.println("                        * * * *                 * * * * * * * * * *                                     ");
+            System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
 
-        Thread.sleep(400);
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                                                                            ");
+            System.out.println(hop+"                                  * * * * * * * *                                                         ");
+            System.out.println(hop+"                              * *                 * * * * *                                               ");
+            System.out.println(hop+"                          * *                               * * * * *                                     ");
+            System.out.println(hop+"                        *                                             *                                   ");
+            System.out.println(hop+"                        *         * * * * * * * *                     *                                   ");
+            System.out.println(hop+"                          * * * *                 * * * * * * * * * *                                     ");
 
-        System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
+            Thread.sleep(400);
 
-        System.out.println("                                                                                            ");
-        System.out.println("                                                                                            ");
-        System.out.println("                       * * * * * * * * * * * * * * * * * * * * * * * * * *                             ");
-        System.out.println("                     *                                                     *                           ");
-        System.out.println("                     *                                                     *                           ");
-        System.out.println("                       * * * * * * * * * * * * * * * * * * * * * * * * * *                             ");
+            System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
 
-        Thread.sleep(400);
+            i++;
 
-        System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
-
-        i++;
-
-        }
+            }
        
     }
     
