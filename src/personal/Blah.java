1c8ad4e92ee693a4085079582b3ca4b851950941
JoseChapela <jchappast@ieschandomonte.edu.es>
2024-11-15 14:06:49 +0100

    15/11_14:06

--- src/personal/Blah.java
+++
@@ -1,9 +0,0 @@
-package personal;
-
-public class Blah {
-    public static void main(String[] args) {
-        double num = 2.3;
-        System.out.printf("%.2f", num);
-    }
-    
-}
--- src/ud2/ejerciciosGrandes/calculadoraHumana.java
+++ src/ud2/ejerciciosGrandes/calculadoraHumana.java
@@ -1,39 +1,74 @@
 package ud2.ejerciciosGrandes;
 
+import java.util.Scanner;
+
 public class calculadoraHumana {
 
-    static final int NUM_MAX = 100;
+    static final int OPERANDO_MAX = 20, RESULTADO_MAX = 200;
+
     public static void main(String[] args) {
 
-        final int NUM_OPERACIONES = 8;
+        final int NUM_OPERACIONES = 7;
 
-        int num1, num2;
-        
+        int num1, num2, resultado, respuesta, aciertos = 0, fallos = 0;
+        char signo;
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("CALCULADORA HUMANA");
 
-        for (int i = 0 ; i < NUM_OPERACIONES -1 ; i++ ) {
+        num1 = numeroRandom(OPERANDO_MAX);
 
-            num1 = numeroRandom(NUM_MAX);
-            num2 = numeroRandom(NUM_MAX);
+        for (int i = 0; i < NUM_OPERACIONES ; i++) {
             
-        }     
+            do {
+                
+                num2 = numeroRandom(OPERANDO_MAX);
+                signo = signoAleatorio();
+                resultado = operation(num1, num2, signo);
+            } while (resultado > RESULTADO_MAX || resultado < 0);
+
+            System.out.println("******************************");
+            System.out.println(num1 + "" + signo + "" + num2 + " ?");
+            System.out.println("******************************");
+            System.out.println();
+            respuesta = sc.nextInt();
+
+            num1 = num2;
+
+            if (respuesta == resultado) {
+
+                aciertos++;
+                System.out.println("HAS ACERTADO");
+                System.out.println("Llevas acertados "+aciertos);
+
+            } else {
+
+                fallos++;
+                System.out.println("HAS FALLADO");
+                System.out.println("Llevas fallados "+fallos);
+
+            }
+
+        }
+        sc.close();
 
     }
 
-    public static int numeroRandom(int limiteMax){
+    public static int numeroRandom(int limiteMax) {
 
-        return (int)Math.random()*(limiteMax - 1) + 1;
+        return (int) (Math.random() * (limiteMax - 1) + 1);
 
     }
 
-    private static char signoAleatorio () {
+    private static char signoAleatorio() {
 
-        int num = (int) Math.random()*3;
-        char signo = num == 0? '+' : num == 1? '-' : num == 2? '*' : '/';
-        return signo;    
+        int num = (int) (Math.random() * 3);
+        char signo = num == 0 ? '+' : num == 1 ? '-' : num == 2 ? '*' : '/';
+        return signo;
     }
 
-    public static int operation (int num1, int num2, char signo){
+    public static int operation(int num1, int num2, char signo) {
 
         switch (signo) {
             case '+':
@@ -49,16 +84,18 @@ public class calculadoraHumana {
                 return num1 * num2;
 
             case '/':
-                while (num2 == 0 || num2 > num1) {
-                    
-                    num2 = numeroRandom(NUM_MAX);
+
+                if (num2 == 0 || num2 > num1 || num1%num2 != 0 ) {
+                    return -1;
+
+                } else {
+                    return num1 / num2;
                 }
-                return num1 / num2;
-        
+
             default:
 
                 return 0;
         }
     }
-    
+
 }
