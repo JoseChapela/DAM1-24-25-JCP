48a8c9f09082f404a9535948d9cc1741bba46c6f
JoseChapela <jchappast@ieschandomonte.edu.es>
2025-01-09 12:51:58 +0100

    09/01

--- src/ud4/EjArrays08.java
+++
@@ -1,7 +0,0 @@
-/*EjArrays08. Leer el nombre y sueldo de 20 empleados y mostrar el nombre y sueldo del empleado que más gana. */
-
-package ud4;
-
-public class EjArrays08 {
-    
-}
--- src/ud4/Empleado.java
+++ src/ud4/Empleado.java
@@ -1,3 +1,5 @@
+/*EjArrays08. Leer el nombre y sueldo de 20 empleados y mostrar el nombre y sueldo del empleado que más gana. */
+
 package ud4;
 
 public class Empleado {
@@ -16,13 +18,18 @@ public class Empleado {
 
     public static void main(String[] args) {
         
-        Empleado[] t = new Empleado[3];
-        Empleado e1 = new Empleado("Juan", 3000);
+        Empleado[] t = new Empleado[20];
+        for (int i = 0; i < 20; i++ ) t[i] = new Empleado("Juan", 1500+i*10);
+        for (int i = 0; i < t.length; i++) System.out.println(t[i].nombre + " "+ t[i].sueldo);
+        
+        
+        /*Empleado e1 = new Empleado("Juan", 3000);
         Empleado e2 = new Empleado("Santiago", 2050);
         Empleado e3 = new Empleado("Javier", 1500);
         t[0] = e1; 
         t[1] = e2;
         t[2] = e3;
+        */
     }
 
 
