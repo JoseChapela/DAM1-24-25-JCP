0e5658a93d37e8df31860caf1d26c461f744f32d
JoseChapela <jchappast@ieschandomonte.edu.es>
2025-02-19 12:59:00 +0100

    0219

--- src/ud5/practicas/rol/Personaje.java
+++ src/ud5/practicas/rol/Personaje.java
@@ -1,5 +1,7 @@
 package ud5.practicas.rol;
 
+import java.util.Random;
+
 public class Personaje {
 
     private String nombre;
@@ -10,16 +12,17 @@ public class Personaje {
     private int constitucion;
     private int nivel = 1;
     private int experiencia = 0;
-    private int hp = 50;
+    private int maxhp = 50;
+    private int hp;
 
     //CONSTRUCTORES
 
-    public Personaje (String nombre, String raza, int fuerza, int agilidad, int constitucion, int nivel, int experiencia, int hp) {
+    public Personaje (String nombre, String raza, int fuerza, int agilidad, int constitucion, int nivel, int experiencia, int maxhp) {
 
-        if (fuerza < 1 || agilidad < 1 || constitucion < 1 || nivel < 1 || experiencia < 0 || hp < 1) throw new IllegalArgumentException("Parámetros no válidos");
+        if (fuerza < 1 || agilidad < 1 || constitucion < 1 || nivel < 1 || experiencia < 0 || maxhp < 1) throw new IllegalArgumentException("Parámetros no válidos");
 
         try {
-            this.raza = Raza.valueOf(raza);
+            this.raza = Raza.valueOf(raza.toUpperCase());
         }
         catch (Exception e) {
             throw new IllegalArgumentException("Parámetros no válidos");
@@ -31,16 +34,50 @@ public class Personaje {
         this.constitucion = constitucion;
         this.nivel = nivel;
         this.experiencia = experiencia;
-        this.hp = hp;
+        this.maxhp = maxhp;
     }
 
     public Personaje (String nombre, String raza, int fuerza, int agilidad, int constitucion) {
 
-        this.hp += constitucion;
+        this(nombre, raza, fuerza, agilidad, constitucion, fuerza, agilidad, constitucion);
+        this.maxhp += constitucion;
     }
 
-    static comprobarRaza () {
-        
+    public Personaje (String nombre, String raza) {
+
+        this(nombre, raza, intAleatorioEntre(1, 100), intAleatorioEntre(1, 100), intAleatorioEntre(1, 100));
+    }
+
+    public Personaje (String nombre) {
+
+        this(nombre, "HUMANO");
+    }
+
+    //MÉTODOS PÚBLICOS
+
+    public void mostrar () {
+
+        System.out.println("INFORMACIÓN DE PERSONAJE");
+        System.out.println();
+        System.out.println("Nombre: " + this.nombre + "   Nvl" + this.nivel);
+        System.out.println("Raza: " + this.raza.toString());
+        System.out.println("Fuerza: " + this.fuerza);
+        System.out.println("Agilidad: " + this.agilidad);
+        System.out.println("Constitucion: " + this.constitucion);
+        System.out.println("Vida: " + hp + "/" + this.maxhp);
+    }
+
+    public String toString () {
+
+        return String.format(this.nombre + "  (" + hp + "/" + maxhp + ")" );
+    }
+
+    //MÉTODOS PRIVADOS
+
+    private static int intAleatorioEntre (int inicio, int fin) {
+
+        Random rnd = new Random();
+        return rnd.nextInt(inicio, fin+1);
     }
 
 }
